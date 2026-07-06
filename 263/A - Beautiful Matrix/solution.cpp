76210aa2a9bcc255ac9e39d7d32648fba1feff18
Sree Charan <sreecharanpaila26@gmail.com>
2026-07-06 22:22:05 +0530

    Add Beautiful Matrix [A] from Codeforces

---
+++ 263/A - Beautiful Matrix/solution.cpp
@@ -0,0 +1,21 @@
+#include<bits/stdc++.h>
+using namespace std;
+int main(){
+    int mat[6][6];
+    for(int i=1;i<=5;i++){
+        for(int j=1;j<=5;j++){
+            cin>>mat[i][j];
+        }
+    }
+    for(int i=1;i<=5;i++){
+        for(int j=1;j<=5;j++){
+            cin>>mat[i][j];
+            if(mat[i][j]==1){
+                cout<<abs(i-3)+abs(j-3);
+                return 0;
+            }
+        }
+        
+    }
+    return 0;
+}
