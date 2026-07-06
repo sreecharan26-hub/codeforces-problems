b051448b7986b40c2bd5ff455131ae324f3d911c
Sree Charan <sreecharanpaila26@gmail.com>
2026-07-06 22:21:06 +0530

    Add Watermelon [A] from Codeforces

---
+++ 4/A - Watermelon/solution.cpp
@@ -0,0 +1,13 @@
+#include<bits/stdc++.h>
+using namespace std;
+int main(){
+    int m;
+    cin>>m;
+    if(m%2==0 and m>2){
+        cout<<"YES";
+    }
+    else{
+        cout<<"NO";
+    }
+    return 0;
+}
