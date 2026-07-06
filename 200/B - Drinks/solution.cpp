a5c6ae2c725d38506a456329107215c6a6ef5009
Sree Charan <sreecharanpaila26@gmail.com>
2026-07-06 22:22:17 +0530

    Add Drinks [B] from Codeforces

---
+++ 200/B - Drinks/solution.cpp
@@ -0,0 +1,14 @@
+#include<bits/stdc++.h>
+using namespace std;
+int main(){
+    int n;
+    cin>>n;
+    double sum =0;
+    for(int i=0;i<n;i++){
+        double x;
+        cin>>x;
+        sum+=x;
+    }
+    cout<<sum/n;
+    return 0;
+}
