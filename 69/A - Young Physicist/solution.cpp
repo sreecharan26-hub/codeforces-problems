f40af24c52cdbc386f5d5dd50d6dc8fee27527d1
Sree Charan <sreecharanpaila26@gmail.com>
2026-07-06 22:21:51 +0530

    Add Young Physicist [A] from Codeforces

---
+++ 69/A - Young Physicist/solution.cpp
@@ -0,0 +1,24 @@
+#include<bits/stdc++.h>
+using namespace std;
+int main(){
+    int n;
+    cin>>n;
+    int a=0,b=0,c=0;
+    while(n--){
+        int x,y,z;
+        cin>>x>>y>>z;
+        
+        a+=x;
+        b+=y;
+        c+=z;
+    }
+        
+    if(a==0 and b==0 and c==0){
+        cout<<"YES";
+    }
+    else{
+        cout<<"NO";
+    }
+        
+    return 0;
+}
