f02388450f7e66e7573f89b9ea953dbab4581d84
Sree Charan <sreecharanpaila26@gmail.com>
2026-07-28 13:29:24 +0530

    Add IQ test [A] from Codeforces

--- 25/A - IQ test/solution.cpp
+++ 25/A - IQ test/solution.cpp
@@ -1,37 +1,36 @@
-#include<bits/stdc++.h>
+#include <bits/stdc++.h>
 using namespace std;
-int main(){
-    int n ;
-    cin>>n;
-    vector<int>v(n);
-    for(auto &i : v){
-        cin>>i;
+ 
+int main() {
+    int n;
+    cin >> n;
+ 
+    vector<int> a(n);
+    int even = 0, odd = 0;
+ 
+    for (int i = 0; i < n; i++) {
+        cin >> a[i];
+        if (a[i] % 2 == 0)
+            even++;
+        else
+            odd++;
     }
-    int ocnt = 0;
-    int ecnt = 0;
-    for(int i=0;i<n;i++){
-        if(v[i]%2==0){
-            ecnt++;
+ 
+    if (odd == 1) {
+        for (int i = 0; i < n; i++) {
+            if (a[i] % 2 != 0) {
+                cout << i + 1;
+                break;
+            }
         }
-        else{
-            ocnt++;
-        }
-    }
-    if(ecnt==1){
-        for(int i=0;i<n;i++){
-            if(v[i]%2==0){
-                cout<<i+1;
+    } else {
+        for (int i = 0; i < n; i++) {
+            if (a[i] % 2 == 0) {
+                cout << i + 1;
                 break;
             }
         }
     }
-    if(ocnt==1){
-        for(int i =0;i<n;i++){
-            if(v[i]%2!=0){
-                cout<<i+1;
-                break;
-            }
-        }
-    }
-    
+ 
+    return 0;
 }
