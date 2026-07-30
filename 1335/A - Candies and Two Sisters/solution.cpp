9e08dfb35f49989fecfd5b4f6deb3ce6ff6184c9
Sree Charan <sreecharanpaila26@gmail.com>
2026-07-30 21:09:07 +0530

    Add Candies and Two Sisters [A] from Codeforces

---
+++ 1335/A - Candies and Two Sisters/solution.cpp
@@ -0,0 +1,18 @@
+#include<bits/stdc++.h>
+using namespace std;
+int main(){
+    int t;
+    cin>>t;
+    while(t--){
+        long long n;
+        cin>>n;
+        
+        if(n<=2){
+            cout<<0<<endl;
+        }
+        else{
+            cout<<(n-1)/2<<endl;
+        }
+    }
+    return 0;
+}
